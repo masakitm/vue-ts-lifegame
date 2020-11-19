243826b3f7c5a081854fb4368b4596d6c34eca7f
tomotsuka.masaki <tomotsuka.masaki@pocke.co.jp>
2020-11-19 18:56:52 +0900

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Board />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Board from "./components/Board.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Board
+  }
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Board.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="cell">
+        <template v-if="item.live">
+          ■
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import useLifeGame from "../hooks/useLifeGame";
+
+export default {
+  setup() {
+    return {
+      ...useLifeGame()
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
